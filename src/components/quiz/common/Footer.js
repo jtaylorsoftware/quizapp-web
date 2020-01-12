39c18a51b31e6cebc449f45468242957e0b2af0f
jeremyt135 <jdt13579@gmail.com>
2020-01-11 21:36:57 -0800

    Add Footer for quiz pages

---
+++ src/components/quiz/common/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer class='footer bg-light'>
+      <div class='container h-100'>
+        <div class='col-lg-8 col-sm-10 mx-auto h-100 d-flex align-items-center'>
+          <button class='btn btn-success ml-auto'>Save</button>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
