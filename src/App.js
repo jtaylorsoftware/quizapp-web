2b4319d414d3507785058fd2d3427543412e9f47
jeremyt135 <jdt13579@gmail.com>
2020-01-12 00:15:54 -0800

    Add redux Provider

--- src/App.js
+++ src/App.js
@@ -10,21 +10,25 @@ import QuizBrowser from './components/quiz/browse/QuizBrowser'
 import QuizEditor from './components/quiz/editor/QuizEditor'
 import Dashboard from './components/user/Dashboard'
 import Quiz from './components/quiz/answer/Quiz'
+import { Provider } from 'react-redux'
+import store from './store/store'
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path='/' component={Landing} />
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/dashboard' component={Dashboard} />
-        <Route exact path='/quiz/public' component={QuizBrowser} />
-        <Route exact path='/quiz/create' component={QuizEditor} />
-        <Route exact path='/quiz/:id' component={Quiz} />
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/dashboard' component={Dashboard} />
+          <Route exact path='/quiz/public' component={QuizBrowser} />
+          <Route exact path='/quiz/create' component={QuizEditor} />
+          <Route exact path='/quiz/:id' component={Quiz} />
+        </Switch>
+      </Router>
+    </Provider>
   )
 }
 
