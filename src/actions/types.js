9181bb6d06bf8027cdaab8c3797659652ffbc4d5
jeremyt135 <jdt13579@gmail.com>
2020-01-12 11:22:51 -0800

    Add login actions

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,13 +1,17 @@
 const ActionTypes = {
   Auth: {
-    // User has successfully registered
+    // User has successfully registered, action data contains JWT
     REGISTER_USER: 'REGISTER_USER',
     // Error registering user
     REGISTER_ERROR: 'REGISTER_ERROR',
-    // User has been authenticated by the server
+    // User data received from server, contained in action data
     AUTH_USER: 'AUTH_USER',
     // Error authenticaing user
-    AUTH_ERROR: 'AUTH_ERROR'
+    AUTH_ERROR: 'AUTH_ERROR',
+    // User has logged in, action data contains JWT
+    LOGIN_USER: 'LOGIN_USER',
+    // UError logging user in
+    LOGIN_ERROR: 'LOGIN_ERROR'
   }
   // Edit: {
   //   EDIT_TITLE: 'EDIT_TITLE',
