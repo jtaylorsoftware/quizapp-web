cdd80bb9d3afc5bf4222cd4315c4f2a6d593c08d
jeremyt135 <jdt13579@gmail.com>
2020-01-12 11:23:08 -0800

    Add login action creator

--- src/actions/user/auth.js
+++ src/actions/user/auth.js
@@ -57,3 +57,34 @@ export const register = (username, email, password) => async dispatch => {
     })
   }
 }
+
+/**
+ * Logs a User in, fetching their JWT
+ * Dispatches an action of type LOGIN_USER on success and LOGIN_ERROR otherwise.
+ * @param {string} email
+ * @param {string} password
+ */
+export const login = (email, password) => async dispatch => {
+  try {
+    const response = await fetch('/api/auth', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    })
+    if (response.ok) {
+      const data = await response.json()
+      dispatch({
+        type: ActionTypes.Auth.LOGIN_USER,
+        data
+      })
+      dispatch(loadUser())
+    }
+  } catch (error) {
+    console.error(error)
+    dispatch({
+      type: ActionTypes.Auth.LOGIN_ERROR
+    })
+  }
+}
