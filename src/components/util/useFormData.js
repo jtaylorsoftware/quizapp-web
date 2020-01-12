6cfb735789dc91ea2ea84dd700908faf0289a9fd
jeremyt135 <jdt13579@gmail.com>
2020-01-12 00:40:39 -0800

    Add documentation

--- src/components/util/useFormData.js
+++ src/components/util/useFormData.js
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 
+/**
+ * Custom React hook that creates a formData state and a method
+ * for handling value changes by form input.
+ * @param {*} initialState
+ * @returns {[any, function(Event): void]} The current formData and the update handler
+ */
 export const useFormData = initialState => {
   const [formData, setFormData] = useState(initialState)
   return [
