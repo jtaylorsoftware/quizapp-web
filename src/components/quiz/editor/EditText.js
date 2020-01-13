91065d7ad39e6594d785a31e95b40b6f9c117f49
jeremyt135 <jdt13579@gmail.com>
2020-01-12 19:26:10 -0800

    Add generic text editor component
    Component can be used for editing any Quiz text (title, question, answer) but might want TextArea for those for clarity.

---
+++ src/components/quiz/editor/EditText.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+const EditText = ({ editName, text, handleChange }) => {
+  const [isEditing, setIsEditing] = useState(false)
+
+  return (
+    <>
+      <div className='row mb-2'>
+        <div className='col'>
+          <input
+            type='text'
+            className='form-control form-control-lg mb-0'
+            name={editName}
+            value={text}
+            onChange={handleChange}
+            readOnly={!isEditing}
+          />
+        </div>
+      </div>
+      <div className='row mb-4'>
+        {isEditing ? (
+          <>
+            <div className='col d-flex align-items-center justify-content-start'>
+              <button
+                type='button'
+                className='btn btn-success btn-sm text-capitalize'
+                onClick={() => setIsEditing(false)}>
+                Confirm
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className='col d-flex align-items-center justify-content-start'>
+            <button
+              type='button'
+              className='btn btn-info btn-sm text-capitalize'
+              onClick={() => setIsEditing(true)}>
+              Edit {editName}
+            </button>
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
+
+EditText.propTypes = {
+  editName: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired
+}
+
+export default EditText
