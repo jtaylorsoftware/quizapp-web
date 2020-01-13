dfe79a1e18583b11ab5878f16c6d0803b9223b98
jeremyt135 <jdt13579@gmail.com>
2020-01-13 14:19:50 -0800

    Refactor state and add label
    Refactored component to manage its own separate state for the selected dateTime. Now uses the setQuizData prop to merge its changes with the editor

--- src/components/quiz/editor/DateTimePicker.js
+++ src/components/quiz/editor/DateTimePicker.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import flatpickr from 'flatpickr'
 import 'flatpickr/dist/flatpickr.min.css'
@@ -7,46 +7,68 @@ import PropTypes from 'prop-types'
 import '../../../styles/datepicker.css'
 import moment from 'moment'
 
+// Format string accepted by flatpickr for displaying the date
+const flatpickrFormat = 'm-d-Y h:i K'
+// Moment format string that is analogous to the flatpickr format
+const momentFormat = 'MM-DD-YYYY h:mm A'
+
 /**
  * Displays an input for the user to select a date and time.
  */
-const DateTimePicker = ({ defaultDate, setDate }) => {
+const DateTimePicker = ({ label, defaultValue, minValue, setQuizData }) => {
+  const [dateTime, setDateTime] = useState(moment(defaultValue))
+
   const inputRef = useRef(null)
 
   useEffect(() => {
-    const date = moment(defaultDate)
-    const dateDisplayFormat = 'm-d-Y h:i K'
-    const dateValueFormat = 'MM-DD-YYYY h:mm A'
     const datePicker = flatpickr(inputRef.current, {
-      dateFormat: dateDisplayFormat,
-      defaultDate: date.format(dateValueFormat).toString(),
-      defaultHour: date.hours(),
-      defaultMinute: date.minutes(),
+      dateFormat: flatpickrFormat,
+      defaultDate: dateTime.format(momentFormat).toString(),
+      minDate: moment(minValue)
+        .format(momentFormat)
+        .toString(),
       enableTime: true,
+      minuteIncrement: 1,
       onClose: (_, dateStr) => {
-        setDate(moment(dateStr, dateValueFormat).toISOString())
+        setQuizData({ expiresIn: moment(dateStr, momentFormat).toISOString() })
+        setDateTime(moment(dateStr, momentFormat))
       }
     })
     return () => datePicker.destroy()
   })
   return (
-    <>
-      <input
-        type='text'
-        className='form-control'
-        id='expirationPicker'
-        placeholder='Select date and time'
-        ref={inputRef}
-        readOnly={true}
-      />
-      <small className='text-muted'>Click to change date and time</small>
-    </>
+    <div className='row mb-4'>
+      <div className='col'>
+        <div className='row'>
+          <div className='col'>
+            <label htmlFor='dateTimePicker'>
+              {(label || 'Pick a date and time') + ':'}
+            </label>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <input
+              type='text'
+              className='form-control'
+              id='dateTimePicker'
+              placeholder='Select date and time'
+              ref={inputRef}
+              readOnly={true}
+            />
+            <small className='text-muted'>Click to change date and time</small>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
 DateTimePicker.propTypes = {
-  defaultDate: PropTypes.string.isRequired,
-  setDate: PropTypes.func.isRequired
+  label: PropTypes.string,
+  defaultValue: PropTypes.string.isRequired,
+  minValue: PropTypes.string.isRequired,
+  setQuizData: PropTypes.func.isRequired
 }
 
 export default DateTimePicker
