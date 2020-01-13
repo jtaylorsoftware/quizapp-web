361bc1f1ce60b2d692bf2236ea621bfb2f224621
jeremyt135 <jdt13579@gmail.com>
2020-01-12 17:45:00 -0800

    Install third party dependencies
    Change to using bootstrap, jquery, popper as bundled dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import 'jquery/dist/jquery.slim'
+import 'popper.js/dist/umd/popper.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
