4bfb58d508ad9fc31e4ae300a1e138a153369c0c
jeremyt135 <jdt13579@gmail.com>
2020-01-14 12:48:55 -0800

    Integrate with redux-persist

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,26 @@
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import { auth } from './user/auth'
 import { user } from './user/user'
+import { quiz } from './quiz/quiz'
 
-export default combineReducers({
-  auth,
-  user
+const rootPersistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['auth']
+}
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  blacklist: ['token']
+}
+
+const rootReducer = combineReducers({
+  auth: persistReducer(authPersistConfig, auth),
+  user,
+  quiz
 })
+
+export default persistReducer(rootPersistConfig, rootReducer)
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,17 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore } from 'redux-persist'
+
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction'
 
 import rootReducer from '../reducers/index'
 
-export default createStore(
-  rootReducer,
-  {},
-  composeWithDevTools(applyMiddleware(thunk))
-)
+export default () => {
+  let store = createStore(
+    rootReducer,
+    {},
+    composeWithDevTools(applyMiddleware(thunk))
+  )
+  let persistor = persistStore(store)
+  return { store, persistor }
+}
