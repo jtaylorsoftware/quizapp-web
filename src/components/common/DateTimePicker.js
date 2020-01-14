2644fc98239266cca4b1e31680f36b7042e70735
jeremyt135 <jdt13579@gmail.com>
2020-01-13 16:27:46 -0800

    Move DateTimePicker to common and refactor
    Refactored to only render the input without rows and cols

--- src/components/common/DateTimePicker.js
+++ src/components/common/DateTimePicker.js
@@ -4,7 +4,7 @@ import flatpickr from 'flatpickr'
 import 'flatpickr/dist/flatpickr.min.css'
 import PropTypes from 'prop-types'
 
-import '../../../styles/datepicker.css'
+import '../../styles/datepicker.css'
 import moment from 'moment'
 
 // Format string accepted by flatpickr for displaying the date
@@ -15,7 +15,7 @@ const momentFormat = 'MM-DD-YYYY h:mm A'
 /**
  * Displays an input for the user to select a date and time.
  */
-const DateTimePicker = ({ label, defaultValue, minValue, setQuizData }) => {
+const DateTimePicker = ({ id, defaultValue, minValue, onChange }) => {
   const [dateTime, setDateTime] = useState(moment(defaultValue))
 
   const inputRef = useRef(null)
@@ -30,45 +30,29 @@ const DateTimePicker = ({ label, defaultValue, minValue, setQuizData }) => {
       enableTime: true,
       minuteIncrement: 1,
       onClose: (_, dateStr) => {
-        setQuizData({ expiresIn: moment(dateStr, momentFormat).toISOString() })
         setDateTime(moment(dateStr, momentFormat))
+        onChange(moment(dateStr, momentFormat).toISOString())
       }
     })
     return () => datePicker.destroy()
   })
   return (
-    <div className='row mb-4'>
-      <div className='col'>
-        <div className='row'>
-          <div className='col'>
-            <label htmlFor='dateTimePicker'>
-              {(label || 'Pick a date and time') + ':'}
-            </label>
-          </div>
-        </div>
-        <div className='row'>
-          <div className='col'>
-            <input
-              type='text'
-              className='form-control'
-              id='dateTimePicker'
-              placeholder='Select date and time'
-              ref={inputRef}
-              readOnly={true}
-            />
-            <small className='text-muted'>Click to change date and time</small>
-          </div>
-        </div>
-      </div>
-    </div>
+    <input
+      type='text'
+      className='form-control date-time-picker'
+      id={id}
+      placeholder='Select date and time'
+      ref={inputRef}
+      readOnly={true}
+    />
   )
 }
 
 DateTimePicker.propTypes = {
-  label: PropTypes.string,
+  id: PropTypes.string,
   defaultValue: PropTypes.string.isRequired,
   minValue: PropTypes.string.isRequired,
-  setQuizData: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired
 }
 
 export default DateTimePicker
