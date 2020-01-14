0ebc7b9b629825f2fa4b01da29a63f7214208895
jeremyt135 <jdt13579@gmail.com>
2020-01-14 12:47:32 -0800

    Add text and onClick props

--- src/components/quiz/common/Footer.js
+++ src/components/quiz/common/Footer.js
@@ -1,15 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Footer = () => {
+/**
+ * Displays a footer with a single action button (likely for saving answers or submitting a quiz)
+ * @param {object} props Component props
+ * @param {string} props.text Text to display on the button
+ * @param {function} props.onClick Function to invoke when clicked
+ */
+const Footer = ({ text, onClick }) => {
   return (
     <footer className='footer bg-light'>
       <div className='container h-100'>
         <div className='col-lg-8 col-sm-10 mx-auto h-100 d-flex align-items-center'>
-          <button className='btn btn-success ml-auto'>Save</button>
+          <button className='btn btn-success ml-auto' onClick={onClick}>
+            {text}
+          </button>
         </div>
       </div>
     </footer>
   )
+}
+
+Footer.propTypes = {
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
 }
 
 export default Footer
