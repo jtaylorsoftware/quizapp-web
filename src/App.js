3ebd3f9608e353a7d15c47cf341c869cc7bbc060
jeremyt135 <jdt13579@gmail.com>
2020-01-16 18:40:54 -0800

    Remove QuizBrowser

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Landing from './components/layout/Landing'
 import Register from './components/user/Register'
 import Login from './components/user/Login'
 
-import QuizBrowser from './components/quiz/browse/QuizBrowser'
 import QuizEditor from './components/quiz/editor/QuizEditor'
 import Dashboard from './components/user/dashboard/Dashboard'
 import Quiz from './components/quiz/answer/Quiz'
@@ -31,7 +30,6 @@ const App = () => {
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
             <PrivateRoute exact path='/dashboard' component={Dashboard} />
-            <Route exact path='/quiz/public' component={QuizBrowser} />
             <PrivateRoute exact path='/quiz/create' component={QuizEditor} />
             <Route exact path='/quiz/:id' component={Quiz} />
             <PrivateRoute exact path='/quiz/:id/edit' component={QuizEditor} />
