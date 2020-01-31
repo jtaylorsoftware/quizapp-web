db0dd6e6e9a05c2947115aed3bf117160e46a81a
jeremyt135 <jdt13579@gmail.com>
2020-01-30 17:36:48 -0800

    Disable redux devtools in production

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { persistStore } from 'redux-persist'
 
 import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 
 import rootReducer from '../reducers/index'
 
