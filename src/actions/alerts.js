616a61287e51b176763a0b4d0444a072ebd25b6b
jeremyt135 <jdt13579@gmail.com>
2020-02-05 11:48:11 -0800

    Reduce time on screen

--- src/actions/alerts.js
+++ src/actions/alerts.js
@@ -1,6 +1,9 @@
 import { Alerts } from './types'
 import uuidv4 from 'uuid/v4'
 
+const SECONDS_TO_MS = 1000
+const TIME_ON_SCREEN = 3 * SECONDS_TO_MS
+
 export const setAlert = ({ msg, type }) => dispatch => {
   const id = uuidv4()
   dispatch({
@@ -17,6 +20,6 @@ export const setAlert = ({ msg, type }) => dispatch => {
         type: Alerts.CLEAR_ALERT,
         data: id
       }),
-    5 * 1000 // remove after a few seconds
+    TIME_ON_SCREEN
   )
 }
