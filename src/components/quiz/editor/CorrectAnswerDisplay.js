a0d7e983feb98556ad1e10f907e98093f20cbedd
jeremyt135 <jdt13579@gmail.com>
2020-02-05 15:02:27 -0800

    Add small instruction text for changing answer

--- src/components/quiz/editor/CorrectAnswerDisplay.js
+++ src/components/quiz/editor/CorrectAnswerDisplay.js
@@ -3,11 +3,21 @@ import PropTypes from 'prop-types'
 
 const CorrectAnswerDisplay = ({ number }) => {
   return (
-    <div className='row mb-2'>
-      <div className='col-sm-6 mr-auto'>
-        <h6 className='px-2'>Correct Answer: {number}</h6>
+    <>
+      <div className='row mb-0'>
+        <div className='col'>
+          <h6 className='px-2 mb-0'>Correct Answer: {number}</h6>
+        </div>
       </div>
-    </div>
+
+      <div className='row mb-2'>
+        <div className='col'>
+          <small className='px-2 text-muted'>
+            Click an answer's radio box to change the correct answer
+          </small>
+        </div>
+      </div>
+    </>
   )
 }
 
