116daec12885b71bfa8746484902836ea534e1d5
jeremyt135 <jdt13579@gmail.com>
2020-10-12 14:36:29 -0700

    Remove log statement

--- src/store/alerts/reducers.ts
+++ src/store/alerts/reducers.ts
@@ -6,11 +6,6 @@ export function alertReducer(
   state = initialState,
   action: AlertActionTypes
 ): AlertState {
-  console.log(
-    `alertReducer ${action.type} ${
-      action.type === CLEAR_ALERT ? action.id : ''
-    }`
-  )
   switch (action.type) {
     case SET_ALERT:
       return [...state, action.payload]
