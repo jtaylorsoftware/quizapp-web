c44fb6107b41c281ba1cb0658425893311a22e6f
jeremyt135 <jdt13579@gmail.com>
2020-10-13 12:46:23 -0700

    Move Provider and Router to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { BrowserRouter as Router } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux'
+
 import 'jquery/dist/jquery.slim'
 import 'popper.js/dist/umd/popper.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -9,5 +13,18 @@ import 'flatpickr/dist/flatpickr.min.css'
 import './styles/theme.scss'
 
 import App from './App'
+import createStore from './store/store'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const { store, persistor } = createStore()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
+  </Provider>,
+
+  document.getElementById('root')
+)
