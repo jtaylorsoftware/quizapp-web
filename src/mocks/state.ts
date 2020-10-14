aa72073c1e338d2aff1333d621788cd1093bf448
jeremyt135 <jdt13579@gmail.com>
2020-10-14 13:18:25 -0700

    Add helpers for redux state mock

---
+++ src/mocks/state.ts
@@ -0,0 +1,77 @@
+import moment from 'moment'
+import { AuthState } from 'store/auth/types'
+import { QuizState } from 'store/quiz/types'
+import { ResultListingsState } from 'store/quizresults/types'
+import { ResultState } from 'store/result/types'
+import { UserState } from 'store/user/types'
+
+const now = moment().toISOString()
+const expiration = moment(now).add(1, 'd').toISOString()
+
+const id = (t: string, n: number) => `${t}-id-${n}`
+const userid = (n: number) => id('user', n)
+const quizid = (n: number) => id('quiz', n)
+const resultid = (n: number) => id('result', n)
+
+const username = (n: number) => `username-${n}`
+
+export const auth: AuthState = {
+  token: '',
+  isAuthenticated: false
+}
+export const user: UserState = {
+  loading: false,
+  error: null,
+  user: {
+    _id: userid(0),
+    date: 'date',
+    username: username(0),
+    email: 'email@email.com',
+    quizzes: [],
+    results: []
+  }
+}
+export const quiz: QuizState = {
+  loading: false,
+  error: null,
+  quiz: {
+    _id: quizid(0),
+    date: now,
+    user: username(0),
+    title: 'quiz0',
+    expiration: expiration,
+    questions: [{ text: 'Q1', answers: [{ text: 'A1' }, { text: 'A2' }] }]
+  }
+}
+
+export const quizResults: ResultListingsState = {
+  loading: false,
+  error: null,
+  results: [
+    {
+      _id: resultid(0),
+      date: now,
+      user: userid(1),
+      quiz: quizid(0),
+      quizOwner: username(0),
+      score: 0,
+      username: username(1)
+    }
+  ]
+}
+export const result: ResultState = {
+  loading: false,
+  error: null,
+  result: {
+    _id: resultid(0),
+    date: now,
+    user: userid(1),
+    quiz: quizid(0),
+    quizOwner: userid(0),
+    answers: [{ choice: 0, isCorrect: false, correctAnswer: 1 }],
+    score: 0,
+    quizTitle: 'quiz0',
+    ownerUsername: username(0),
+    username: username(1)
+  }
+}
