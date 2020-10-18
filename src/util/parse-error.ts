57abd125e0bf6c61038267cbf7d4d82b33430f4a
jeremyt135 <jdt13579@gmail.com>
2020-10-17 23:20:50 -0700

    Use ApiError in return type

--- src/util/parse-error.ts
+++ src/util/parse-error.ts
@@ -1,4 +1,6 @@
-export const parseError = async (response: Response) => {
+import { ApiError } from 'api'
+
+export const parseError = async (response: Response): Promise<ApiError> => {
   let errors = []
   const contentType = response.headers.get('Content-Type')
   if (contentType && contentType.startsWith('application/json')) {
