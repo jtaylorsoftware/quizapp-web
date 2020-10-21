3f3cfb06f8094f8a9092b027434040f303f0fcde
jeremyt135 <jdt13579@gmail.com>
2020-10-20 20:41:52 -0700

    Add utilities for time differences
    Added calculateTimeDifference and createTimestamp for determining time differences
    and creating strings describing them.

--- src/util/date.ts
+++ src/util/date.ts
@@ -20,3 +20,36 @@ export const isDateInFuture = (date: Date | string) =>
 
 export const isDateInPast = (date: Date | string) =>
   moment(date).diff(moment()) < 0
+
+export type TimeDifference = {
+  days: number
+  months: number
+  years: number
+}
+
+/**
+ * Calculates the difference of two Moments as `{days, months, years}`
+ */
+export const calculateTimeDifference = (
+  now: moment.Moment,
+  then: moment.Moment
+): TimeDifference => {
+  const diff = moment.duration(now.diff(moment(then)))
+  return { days: diff.days(), months: diff.months(), years: diff.years() }
+}
+
+/**
+ * Returns a string timestamp like `\d+ (years|months|days) ago`
+ */
+export const createTimestamp = ({ days, months, years }: TimeDifference) => {
+  if (years > 0) {
+    return `${years} years ago`
+  }
+  if (months > 0) {
+    return `${months} months ago`
+  }
+  if (days > 0) {
+    return `${days} days ago`
+  }
+  return 'Today'
+}
