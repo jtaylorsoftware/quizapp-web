39d6fa64d2448b690eb9fc4350eba895c600f307
Jeremy Taylor <jeremy.taylor.cs@gmail.com>
2022-03-15 22:05:43 -0700

    Change sr-only to visually-hidden

--- src/components/common/Spinner.tsx
+++ src/components/common/Spinner.tsx
@@ -8,7 +8,7 @@ const Spinner = () => {
   return (
     <Container fluid className="spinner-container">
       <div className="spinner spinner-border" role="status">
-        <span className="sr-only">Loading...</span>
+        <span className="visually-hidden">Loading...</span>
       </div>
     </Container>
   )
